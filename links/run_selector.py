3e57818b62f1f6c9dfae99cf99668944d2260dbf
Jacob Marks <jamarks13@gmail.com>
2023-05-05 15:18:46 -0700

    reducing clutter in output for now

--- links/run_selector.py
+++ links/run_selector.py
@@ -154,27 +154,28 @@ class EvaluationRunSelector(RunSelector):
 
     def generate_compute_run_message(self):
         base_message =  "No evaluation runs found.\n\n"
-        detection_message = "If you want to compute detection evaluation, please run the following command:\n"
-        detection_command = """
-        ```
-        dataset.evaluate_detections(
-            "<det_predictions>",
-            eval_key="eval_det",
-        )
-        ```
-        """
+        return base_message
+        # detection_message = "If you want to compute detection evaluation, please run the following command:\n"
+        # detection_command = """
+        # ```
+        # dataset.evaluate_detections(
+        #     "<det_predictions>",
+        #     eval_key="eval_det",
+        # )
+        # ```
+        # """
 
-        classification_message = "If you want to compute classification evaluation, please run the following command:\n"
-        classification_command = """
-        ```
-        dataset.evaluate_classifications(
-            "<classif_predictions>",
-            eval_key="eval_classif",
-        )
-        ```
-        """
-        message = base_message + detection_message + detection_command + classification_message + classification_command
-        return message
+        # classification_message = "If you want to compute classification evaluation, please run the following command:\n"
+        # classification_command = """
+        # ```
+        # dataset.evaluate_classifications(
+        #     "<classif_predictions>",
+        #     eval_key="eval_classif",
+        # )
+        # ```
+        # """
+        # message = base_message + detection_message + detection_command + classification_message + classification_command
+        # return message
         
     def get_run_info(self, run):
         key = run.key
