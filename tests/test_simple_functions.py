d88be9643eec95b133d3b6be1d6c0cfce6e63e48
Leila Kaneda <leila.kaneda@Leilas-MacBook-Pro.local>
2023-05-09 21:32:26 -0700

    adding test_label

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -18,6 +18,7 @@ def create_view_from_stages(text, dataset):
         all_text += element + "."
     all_text += text[-1]
     code = 'dataset.' + all_text
+    print(code)
     try:
         view = eval(code)
     except:
@@ -37,6 +38,12 @@ def compute_hardness_for_test(dataset):
     fob.compute_hardness(dataset, label_field="my_classifications")
     fob.compute_hardness(dataset, label_field="my_classifications", hardness_field="test_hardness")
 
+def get_categories(view):
+    categories = view.values("ground_truth.detections.label")
+    flat_list = [item for sublist in categories for item in sublist]
+    categories = list(set(flat_list))
+    return categories
+
 class TestClassSimpleFunctions:
     def EvaluateResults(self, ground_truth, gpt_response):
         if gpt_response.stats()['samples_count'] != ground_truth.stats()['samples_count']:
@@ -48,6 +55,8 @@ class TestClassSimpleFunctions:
         elif gpt_response.values("ground_truth.detections.label") != ground_truth.values("ground_truth.detections.label"):
             return False
         elif gpt_response.get_field_schema() != ground_truth.get_field_schema():
+            return False
+        elif get_categories(gpt_response) != get_categories(ground_truth):
             return False
         else:
             return True
@@ -96,6 +105,18 @@ class TestClassSimpleFunctions:
         dataset = foz.load_zoo_dataset("quickstart")
         fob.compute_similarity(dataset, model="clip-vit-base32-torch", brain_key="test_sim")
         expected_view = create_view_from_stages(['sort_by_similarity("lion", k=19)'], dataset)
+        gpt_response = get_gpt_view_text(dataset, prompt)
+        if type(gpt_response) == list:
+            view = create_view_from_stages(gpt_response, dataset)
+            assert self.EvaluateResults(expected_view, view)
+        else:
+            print("Response failed. Returned " + str(gpt_response))
+            assert False
+            
+    def test_label(self):
+        prompt = "Only show labels containing the letter m"
+        dataset = foz.load_zoo_dataset("quickstart")
+        expected_view = create_view_from_stages(['filter_labels("ground_truth", F("label").contains_str("m"))'], dataset)
         gpt_response = get_gpt_view_text(dataset, prompt)
         if type(gpt_response) == list:
             view = create_view_from_stages(gpt_response, dataset)
