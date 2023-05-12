757a2c5d6e52ed347339d7a2c9bba4521c220f56
Ritchie Martori <ritchie@voxel51.com>
2023-05-12 12:30:25 -0700

    add input to gpt op

--- __init__.py
+++ __init__.py
@@ -162,7 +162,6 @@ class CreateViewWithGPT(foo.Operator):
     @property
     def resolve_inputs(self):
         inputs = types.Object()
-        inputs.str("message", label="Message", required=True)
         return types.Property(inputs)
     
     async def execute(self, ctx):
