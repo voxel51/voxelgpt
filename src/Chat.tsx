3e30dfb9b39c23217aca74ba2fa457c1436a5c25
imanjra <ibrahim@voxel51.com>
2023-05-17 13:13:48 -0400

    enhance auto scroll to bottom

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,48 +1,67 @@
-import React, { useEffect, useRef } from 'react'
-import { useRecoilValue } from 'recoil'
-import Message, { MessageWrapper } from './Message'
-import * as state from './state'
-import { ChatGPTAvatar } from './avatars'
-import { Grid } from '@mui/material'
+import { Grid } from "@mui/material";
+import { throttle } from "lodash";
+import React, { useCallback, useEffect, useRef } from "react";
+import { useRecoilValue } from "recoil";
+import { MessageWrapper } from "./Message";
+import { ChatGPTAvatar } from "./avatars";
+import * as state from "./state";
+import { SCROLL_TO_BOTTOM_THROTTLE } from "./constants";
 
 const Chat = () => {
-  const bottomRef = useRef(null)
-  const messages = useRecoilValue(state.atoms.messages)
+  const ref = useRef(null);
+  const bottomRef = useRef(null);
+  const messages = useRecoilValue(state.atoms.messages);
+
+  const scrollToBottom = useCallback(
+    throttle(() => {
+      if (bottomRef.current && messages.length > 0) {
+        bottomRef.current.scrollIntoView({ behavior: "smooth" });
+      }
+    }, SCROLL_TO_BOTTOM_THROTTLE),
+    []
+  );
 
   useEffect(() => {
-    if (bottomRef.current && messages.length > 0) {
-      bottomRef.current.scrollIntoView({ behavior: 'smooth' })
+    const refElem = ref.current;
+    if (refElem) {
+      const refResizeObserver = new ResizeObserver(scrollToBottom).observe(
+        refElem
+      );
+
+      return () => {
+        refResizeObserver.disconnect();
+      };
     }
-  }, [messages])
+  }, []);
 
   const avatars = {
-    incoming: <ChatGPTAvatar />
-  }
+    incoming: <ChatGPTAvatar />,
+  };
 
   return (
-    <div style={{ overflow: 'auto' }}>
+    <div style={{ overflow: "auto" }} ref={ref}>
       <Grid container direction="row">
         {groupMessages(messages, avatars)}
       </Grid>
       <div ref={bottomRef} />
     </div>
-  )
-}
+  );
+};
 
 function groupMessages(messages, avatars) {
-  const els = []
-  let currentGroup = []
-  let idx = 0
+  const els = [];
+  let currentGroup = [];
+  let idx = 0;
   for (const message of messages) {
-    idx += 1
+    idx += 1;
     // group messages by type
     if (currentGroup.length > 0 && currentGroup[0].type !== message.type) {
-      console.log(message)
-      const groupMessage = currentGroup[0]
+      console.log(message);
+      const groupMessage = currentGroup[0];
       els.push(
         <MessageWrapper
           avatar={
-            groupMessage.type === 'incoming'
+            groupMessage.type === "incoming"
               ? avatars.incoming
               : avatars.outgoing
           }
@@ -50,26 +69,26 @@ function groupMessages(messages, avatars) {
           index={idx}
           messages={currentGroup}
         />
-      )
-      currentGroup = [message]
+      );
+      currentGroup = [message];
     } else {
-      currentGroup.push(message)
+      currentGroup.push(message);
     }
   }
   if (currentGroup.length > 0) {
-    const groupMessage = currentGroup[0]
+    const groupMessage = currentGroup[0];
     els.push(
       <MessageWrapper
         avatar={
-          groupMessage.type === 'incoming' ? avatars.incoming : avatars.outgoing
+          groupMessage.type === "incoming" ? avatars.incoming : avatars.outgoing
         }
         type={groupMessage.type}
         index={idx}
         messages={currentGroup}
       />
-    )
+    );
   }
-  return els
+  return els;
 }
 
-export default Chat
+export default Chat;
