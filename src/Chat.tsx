2017e1ec6d5e20898e2cefdc5947692074a32a13
imanjra <ibrahim@voxel51.com>
2023-05-17 10:32:19 -0400

    make examples clickable and enhance light theme

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,60 +1,75 @@
-import React, { useEffect, useRef } from 'react';
-import { useRecoilValue } from 'recoil';
-import Message, { MessageWrapper } from './Message';
-import * as state from './state';
-import { ChatGPTAvatar } from './avatars';
-import {Grid} from "@mui/material";
+import React, { useEffect, useRef } from 'react'
+import { useRecoilValue } from 'recoil'
+import Message, { MessageWrapper } from './Message'
+import * as state from './state'
+import { ChatGPTAvatar } from './avatars'
+import { Grid } from '@mui/material'
 
 const Chat = () => {
-  const bottomRef = useRef(null);
-  const messages = useRecoilValue(state.atoms.messages);
+  const bottomRef = useRef(null)
+  const messages = useRecoilValue(state.atoms.messages)
 
   useEffect(() => {
     if (bottomRef.current && messages.length > 0) {
-      bottomRef.current.scrollIntoView({ behavior: 'smooth' });
+      bottomRef.current.scrollIntoView({ behavior: 'smooth' })
     }
-  }, [messages]);
+  }, [messages])
 
   const avatars = {
     incoming: <ChatGPTAvatar />
   }
 
   return (
-    <div style={{ overflow: 'auto', marginTop: '2rem' }}>
+    <div style={{ overflow: 'auto' }}>
       <Grid container direction="row">
         {groupMessages(messages, avatars)}
       </Grid>
       <div ref={bottomRef} />
     </div>
-  );
-};
+  )
+}
 
 function groupMessages(messages, avatars) {
-  const els = [];
-  let currentGroup = [];
-  let idx = 0;
+  const els = []
+  let currentGroup = []
+  let idx = 0
   for (const message of messages) {
-    idx += 1;
+    idx += 1
     // group messages by type
     if (currentGroup.length > 0 && currentGroup[0].type !== message.type) {
       console.log(message)
       const groupMessage = currentGroup[0]
       els.push(
-        <MessageWrapper avatar={groupMessage.type === 'incoming' ? avatars.incoming : avatars.outgoing} type={groupMessage.type} index={idx} messages={currentGroup} />
-      );
-      currentGroup = [message];
+        <MessageWrapper
+          avatar={
+            groupMessage.type === 'incoming'
+              ? avatars.incoming
+              : avatars.outgoing
+          }
+          type={groupMessage.type}
+          index={idx}
+          messages={currentGroup}
+        />
+      )
+      currentGroup = [message]
     } else {
-      currentGroup.push(message);
+      currentGroup.push(message)
     }
   }
   if (currentGroup.length > 0) {
     const groupMessage = currentGroup[0]
     els.push(
-      <MessageWrapper avatar={groupMessage.type === 'incoming' ? avatars.incoming : avatars.outgoing} type={groupMessage.type} index={idx} messages={currentGroup} />
-    );
+      <MessageWrapper
+        avatar={
+          groupMessage.type === 'incoming' ? avatars.incoming : avatars.outgoing
+        }
+        type={groupMessage.type}
+        index={idx}
+        messages={currentGroup}
+      />
+    )
   }
   return els
 }
 
-
-export default Chat;
+export default Chat
--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,87 +1,109 @@
 import {
+  Avatar,
   Grid,
-  Paper,
-  Typography,
   List,
   ListItem,
-  ListItemText,
   ListItemAvatar,
-  Avatar
-} from '@mui/material';
+  ListItemButton,
+  ListItemText,
+  Paper,
+  Typography
+} from '@mui/material'
 
-import {EmojiObjects, QuestionAnswer, Dataset, Psychology} from "@mui/icons-material";
+import {
+  Dataset,
+  EmojiObjects,
+  Psychology,
+  QuestionAnswer
+} from '@mui/icons-material'
+
+import { useSetRecoilState } from 'recoil'
+import { atoms } from './state'
+
+const examples = [
+  { id: 'example-1', label: 'Show me predicted airplanes' },
+  {
+    id: 'example-2',
+    label: 'Retrieve the first 10 images with 3 dogs and 1 cat'
+  },
+  { id: 'example-3', label: 'How can I integrate GPT-3 into my application?' }
+]
+const capabilities = [
+  {
+    id: 'capability-1',
+    label: 'Understands the schema of your dataset',
+    Icon: EmojiObjects
+  },
+  {
+    id: 'capability-2',
+    label: 'Can run SQL-like queries on computer vision datasets',
+    Icon: Dataset
+  },
+  {
+    id: 'capability-3',
+    label:
+      'Knows how to use brain methods, evaluations, similarity indexes, and more',
+    Icon: Psychology
+  }
+]
 
 export const Intro = () => {
+  const setInput = useSetRecoilState(atoms.input)
   return (
-    <Grid container item direction="row" sx={{margin: 'auto'}} spacing={2} justifyContent="center" alignItems="start">
+    <Grid
+      container
+      item
+      direction="row"
+      sx={{ margin: 'auto' }}
+      spacing={2}
+      justifyContent="center"
+      alignItems="start"
+    >
       <Grid item xs={12}>
-        <Typography variant="h2" style={{textAlign: 'center'}}>
+        <Typography variant="h2" style={{ textAlign: 'center' }}>
           VoxelGPT
         </Typography>
       </Grid>
       <Grid item sm={12} lg={4}>
         <Paper elevation={3} sx={{ padding: '20px', marginBottom: '16px' }}>
-          <Typography variant="h4" gutterBottom style={{textAlign: 'center'}}>
+          <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
             Examples
           </Typography>
           <List>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <QuestionAnswer />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Show me predicted airplanes" />
-            </ListItem>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <QuestionAnswer />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Retrieve the first 10 images with 3 dogs and 1 cat" />
-            </ListItem>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <QuestionAnswer />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="How can I integrate GPT-3 into my application?" />
-            </ListItem>
+            {examples.map(({ label, id }) => (
+              <ListItemButton
+                key={id}
+                onClick={() => {
+                  setInput(label)
+                }}
+              >
+                <ListItemAvatar>
+                  <Avatar>
+                    <QuestionAnswer />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={label} />
+              </ListItemButton>
+            ))}
           </List>
         </Paper>
       </Grid>
       <Grid item sm={12} lg={4}>
         <Paper elevation={3} sx={{ padding: '20px', marginBottom: '16px' }}>
-          <Typography variant="h4" gutterBottom style={{textAlign: 'center'}}>
+          <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
             Capabilities
           </Typography>
           <List>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <EmojiObjects />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Understands the schema of your dataset" />
-            </ListItem>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <Dataset />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Can run SQL-like queries on computer vision datasets" />
-            </ListItem>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <Psychology />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Knows how to use brain methods, evaluations, similarity indexes, and more" />
-            </ListItem>
+            {capabilities.map(({ id, label, Icon }) => (
+              <ListItem key={id}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Icon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={label} />
+              </ListItem>
+            ))}
           </List>
         </Paper>
       </Grid>
