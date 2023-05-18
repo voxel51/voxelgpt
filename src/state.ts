5a81254c61c4797f68fe378da8033a251a27c02b
Ritchie Martori <ritchie@voxel51.com>
2023-05-18 12:16:11 -0700

    polish and loading indicator

--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,18 @@
 import { atom, selector } from 'recoil'
+import {getBrowserStorageEffectForKey} from "@fiftyone/state";
+
+const PLUGIN_NAME = '@voxel51/fiftyone-gpt'
 
 export const atoms = {
   messages: atom({
     key: 'messages',
-    default: []
+    default: [],
+    effects: [
+      getBrowserStorageEffectForKey(`${PLUGIN_NAME}/messages`, {
+        prependDatasetNameInKey: true,
+        useJsonSerialization: true
+      })
+    ]
   }),
   receiving: atom({
     key: 'receiving',
