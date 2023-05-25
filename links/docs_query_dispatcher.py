7174ae67979ba6a5dc008177276a4fbb5029f966
brimoor <brimoor@umich.edu>
2023-05-25 10:04:46 -0400

    RetrievalQA with SOURCES

--- links/docs_query_dispatcher.py
+++ links/docs_query_dispatcher.py
@@ -10,8 +10,11 @@ import pickle
 import re
 import uuid
 
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import DirectoryLoader
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.document_loaders import (
+    DirectoryLoader,
+    UnstructuredMarkdownLoader,
+)
 from langchain.schema import Document, BaseRetriever
 from langchain.text_splitter import TokenTextSplitter
 import numpy as np
@@ -43,6 +46,30 @@ DOC_TYPES = (
     "user_guide",
 )
 
+API_DOC_PATHS = (
+    "brain",
+    "core.plots",
+    "core.session",
+    "core.aggregations",
+    "core.annotation",
+    "core.brain",
+    "core.evaluation",
+    "core.expressions",
+    "core.frame",
+    "core.labels",
+    "core.models",
+    "core.sample",
+    "core.spaces",
+    "core.stages",
+    "core.view",
+    "types",
+)
+
+STANDALONE_DOCS = (
+    "index.html",
+    "release-notes.html",
+)
+
 PATTS_TO_LINKS = {
     "FiftyOne Docs": "https://docs.voxel51.com/",
     "FiftyOne User Guide": "https://docs.voxel51.com/user_guide/index.html",
@@ -55,11 +82,20 @@ PATTS_TO_LINKS = {
 }
 
 
+def _make_api_doc_path(name, docs_dir):
+    return os.path.join(docs_dir, "api", f"fiftyone.{name}.html")
+
+
 def _get_docs_build_dir():
     import fiftyone as fo
 
     fo_repo_dir = os.path.dirname(os.path.dirname(fo.__file__))
     return os.path.join(fo_repo_dir, "docs", "build", "html")
+
+
+def _get_url_from_path(path):
+    rel_path = "".join(path.split("html")[-2:])
+    return f"https://docs.voxel51.com{rel_path}html"
 
 
 def _generate_docs_embeddings():
@@ -69,30 +105,60 @@ def _generate_docs_embeddings():
     release. It requires a source install of FiftyOne with the fresh docs
     build.
     """
-    docs_dir = _get_docs_build_dir()
+
     all_embeddings_dict = {}
 
-    model = get_embedding_function()
-
-    for doc_type in DOC_TYPES:
-        print(f"Generating embeddings for {doc_type}...")
-        doc_type_dir = os.path.join(docs_dir, doc_type)
-
-        loader = DirectoryLoader(doc_type_dir, glob="**/*.html")
+    def add_loader_embeddings(loader, all_embeddings_dict, chunk_size=200):
         documents = loader.load()
-        text_splitter = TokenTextSplitter(chunk_size=200, chunk_overlap=0)
+        text_splitter = TokenTextSplitter(
+            chunk_size=chunk_size, chunk_overlap=0
+        )
         texts = text_splitter.split_documents(documents)
 
         ids = [str(uuid.uuid1()) for _ in texts]
         contents = [text.page_content for text in texts]
+        sources = [
+            _get_url_from_path(text.metadata["source"]) for text in texts
+        ]
         embeddings = model(contents)
 
         curr_embeddings_dict = {
-            id: {"content": content, "embedding": embedding}
-            for id, content, embedding in zip(ids, contents, embeddings)
+            id: {"content": content, "embedding": embedding, "source": source}
+            for id, content, embedding, source in zip(
+                ids, contents, embeddings, sources
+            )
         }
 
         all_embeddings_dict = {**all_embeddings_dict, **curr_embeddings_dict}
+        return all_embeddings_dict
+
+    docs_dir = _get_docs_build_dir()
+    model = get_embedding_function()
+
+    ## STANDALONE DOCS
+    for filename in STANDALONE_DOCS:
+        filepath = os.path.join(docs_dir, filename)
+        loader = UnstructuredMarkdownLoader(filepath)
+        all_embeddings_dict = add_loader_embeddings(
+            loader, all_embeddings_dict
+        )
+
+    ## DOCS CATEGORIES
+    for doc_type in DOC_TYPES:
+        print(f"Generating embeddings for {doc_type}...")
+        doc_type_dir = os.path.join(docs_dir, doc_type)
+        loader = DirectoryLoader(doc_type_dir, glob="**/*.html")
+        all_embeddings_dict = add_loader_embeddings(
+            loader, all_embeddings_dict
+        )
+
+    ## API DOCS
+    for api_doc_path in API_DOC_PATHS:
+        doc = _make_api_doc_path(api_doc_path, docs_dir)
+        loader = UnstructuredMarkdownLoader(doc)
+        all_embeddings_dict = add_loader_embeddings(
+            loader, all_embeddings_dict
+        )
 
     with open(DOCS_EMBEDDINGS_FILE, "wb") as f:
         pickle.dump(all_embeddings_dict, f)
@@ -100,11 +166,15 @@ def _generate_docs_embeddings():
 
 class FiftyOneDocsRetriever(BaseRetriever):
     def __init__(self, embeddings):
+        self.model = get_embedding_function()
         self.contents = [
-            Document(page_content=doc["content"]) for doc in embeddings
+            Document(
+                page_content=doc["content"],
+                metadata={"source": doc["source"]},
+            )
+            for doc in embeddings
         ]
         self.embeddings = [np.array(doc["embedding"]) for doc in embeddings]
-        self.model = get_embedding_function()
 
     def get_relevant_documents(self, query):
         query_embedding = np.array(self.model(query))
@@ -113,7 +183,7 @@ class FiftyOneDocsRetriever(BaseRetriever):
         )
 
         sorted_ix = np.argsort(dists).astype(int)
-        return [self.contents[ix] for ix in sorted_ix[:5]]
+        return [self.contents[ix] for ix in sorted_ix[:10]]
 
     async def aget_relevant_documents(self, query: str):
         raise NotImplementedError
@@ -127,7 +197,7 @@ def _create_docs_retriever():
 
 
 def _create_docs_qa_chain():
-    return RetrievalQA.from_chain_type(
+    return RetrievalQAWithSourcesChain.from_chain_type(
         llm=get_llm(),
         chain_type="stuff",
         retriever=get_docs_retriever(),
@@ -172,7 +242,10 @@ def _format_response(response):
 
 def run_docs_query(query):
     docs_qa = get_docs_qa_chain()
-    response = docs_qa.run([query]).strip()
+    response = docs_qa({"question": query})
+    answer = response["answer"]
+    sources = response["sources"].split(",")
+    response = answer + "Sources:\n" + "\n".join(sources)
     return _format_response(response)
 
 
