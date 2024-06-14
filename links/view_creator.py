0fc684f216d24450190b92e608e4a155f9a2885b
Jacob Marks <jamarks13@gmail.com>
2024-06-13 23:51:27 -0400

    cleanup

--- links/view_creator.py
+++ links/view_creator.py
@@ -9,16 +9,10 @@ View creator.
 import fiftyone as fo
 
 # pylint: disable=relative-beyond-top-level
-from .view_creation_planner import create_view_creation_plan
 from .view_stage_delegator import delegate_view_stage_creation
 from .view_stage_constructor import construct_stage
 from .view_stage_validator import validate_view_stage
 from .utils import has_metadata
-
-
-def write_log(log):
-    with open("/tmp/log.txt", "a") as f:
-        f.write(str(log) + "\n")
 
 
 def create_view_from_plan(sample_collection, view_creation_plan):
@@ -37,7 +31,6 @@ def create_view_from_plan(sample_collection, view_creation_plan):
     for assignee, step in zip(view_creation_actors, view_creation_plan.steps):
         stage = construct_stage(step, assignee, sample_collection)
         stage = validate_view_stage(stage, sample_collection)
-        write_log(str(stage))
         if stage is not None:
             if isinstance(stage, str):
                 impossible_stages.append(step + " - " + stage)
